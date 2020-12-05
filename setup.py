a3a3d6f5fa34c58866336e8afbbbce973e0186fd
Jeff Albrecht <geospatialjeff@gmail.com>
2020-12-04 18:32:45 -0600

    update aiocogeo to 0.3.0 (#7)
    * update setup.py
    
    * update dev extras

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ extras = {
         "pytest-asyncio<0.11.0",
         "pytest-cov",
         "shapely",
-        "botocore==1.15.32",
-        "boto3==1.12.32",
         "aioboto3",
     ]
 }
@@ -42,7 +40,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=[
-        "aiocogeo==0.2.*",
+        "aiocogeo==0.3.*",
         "morecantile",
         "rasterio>=1.1.7",
         "rio-tiler==2.0.0rc2",
